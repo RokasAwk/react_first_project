bed126eb1ba06fdc1c02df05b11436515295fd3a
Rokas <icestar6215@gmail.com>
2023-03-17 22:30:07 +0800

    運用setState搜尋功能

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import { Component } from 'react';
 
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
 
 class App extends Component  {
@@ -8,24 +8,53 @@ class App extends Component  {
   constructor(){
     super();
     this.state = {
-      name: 'Rokas'
+      monsters: []
     }
   }
 
-  render(){ 
+  componentDidMount() {
+    fetch('https://jsonplaceholder.typicode.com/users') // 非同步
+      .then((response) => response.json()) // 轉json
+      .then((users) => this.setState( 
+        () => {
+          return { monsters: users }; // init 賦值給List
+        },
+        () => {
+          console.log(this.state);
+        }
+      ));
+  }
+
+  render(){
+
     return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Hi {this.state.name}
-        </p>
-        <button onClick={()=>{
-        this.setState({ name : '壽司狼' })
-        }}>
-          Change Name
-        </button>
-      </header>
+      <input className='search-box' type='search' placeholder='搜尋怪物...' onChange={(event) => {
+      
+      const searchString = event.target.value.toLocaleLowerCase();
+      const filteredMonsters = this.state.monsters.filter((monster) => {
+        return monster.name.toLocaleLowerCase().includes(searchString);
+       });
+
+      // onChange setState
+      this.setState(() => {
+        return {
+          monsters: filteredMonsters
+        };
+      });   
+
+     }}/>
+
+
+      {
+        //畫面顯示
+        this.state.monsters.map((monster) => {
+          return (
+          <div key={monster.id}><h1>{monster.name}</h1></div>
+          )
+
+        })
+      } 
     </div>
   );} 
  
